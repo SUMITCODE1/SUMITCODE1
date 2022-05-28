0941f08dbdb68ad4edc735416033b6cbb2b10b45
Sumit <sumitsinghindia5@gmail.com>
2022-05-28 23:11:02 +0530

    my first csharp project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace myproject
+{
+    internal class NewBaseType
+    {
+        static void Main(string[] args) => Console.WriteLine("hello sumit");
+    }
+
+    
+}
---
+++ myproject.cs
@@ -0,0 +1,12 @@
+using System;
+namespace name
+{
+   public class myproject{
+       public static void Main(string[]args){
+           Console.WriteLine("Thakur sumit kumar");
+       }
+
+    }
+}
+
+    
